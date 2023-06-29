16f6d998eab62f8a66dfb7af64b017e6e79de02e
genaroibc <bonavitaismael@gmail.com>
2023-06-29 15:41:16 -0300

    feat: add basic list of resources

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+import resources from "@/app/db/UI.json"
+
 export default function Home() {
   return (
     <main className="max-w-7xl mx-auto">
@@ -7,6 +9,18 @@ export default function Home() {
           Resources for developers
         </h2>
       </div>
+
+      <section className="grid grid-cols-[repeat(auto-fill,minmax(300px,1fr))] gap-4 my-20">
+        {resources.map(({ name, description }) => (
+          <article
+            className="flex flex-col gap-4 p-4 bg-slate-800 rounded-md"
+            key={name}
+          >
+            <h5 className="font-semibold text-2xl">{name}</h5>
+            <p className="text-base">{description}</p>
+          </article>
+        ))}
+      </section>
     </main>
   )
 }
