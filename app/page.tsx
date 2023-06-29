a1c228ad7bd186b8f556ea4d03d91a2e0855d741
genaroibc <bonavitaismael@gmail.com>
2023-06-29 19:44:14 -0300

    feat: add new category

--- app/page.tsx
+++ app/page.tsx
@@ -12,6 +12,8 @@ export default function Home() {
         </h2>
       </section>
 
+      <h3 className="font-semibold text-center text-4xl">Categories</h3>
+
       <section className="grid grid-cols-[repeat(auto-fill,minmax(600px,1fr))] gap-4 my-20">
         {categories.map(({ description, name }) => (
           <Category key={name} description={description} name={name} />
