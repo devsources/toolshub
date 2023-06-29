b95ea80b8468c51eecafa479c90b65fa39124b38
genaroibc <bonavitaismael@gmail.com>
2023-06-29 17:47:44 -0300

    feat: list resources in category page

--- app/[category]/page.tsx
+++ app/[category]/page.tsx
@@ -1,4 +1,6 @@
 import { unsluglify } from "@/app/utils/sluglify"
+import resources from "@/app/db/UI.json"
+import { ResourceCard } from "../components/shared/ResourceCard"
 
 type Props = {
   params: { category: string }
@@ -6,8 +8,20 @@ type Props = {
 
 export default function Page({ params: { category } }: Props) {
   return (
-    <main className="max-w-7xl my-20">
+    <main className="max-w-7xl my-20 mx-auto">
       <h1 className="font-bold text-4xl text-center">{unsluglify(category)}</h1>
+
+      <section className="grid grid-cols-[repeat(auto-fill,minmax(300px,1fr))] gap-x-6 gap-y-8 my-20">
+        {resources.map(({ description, name, image, title, url }) => (
+          <ResourceCard
+            key={name}
+            description={description}
+            imageURL={image}
+            title={title}
+            url={url}
+          />
+        ))}
+      </section>
     </main>
   )
 }
--- app/components/shared/ResourceCard.tsx
+++ app/components/shared/ResourceCard.tsx
@@ -1,3 +1,5 @@
+import { IconExternalLink } from "@tabler/icons-react"
+
 type Props = {
   title: string
   description: string
@@ -8,12 +10,20 @@ type Props = {
 export function ResourceCard({ description, imageURL, title, url }: Props) {
   return (
     <article className="flex flex-col gap-4 rounded-md hover:scale-105 hover:shadow-2xl bg-slate-700 p-4 max-w-xs transition-transform">
-      <img src={imageURL} alt={title} className="rounded-md" />
+      <img
+        src={imageURL}
+        alt={title}
+        className="rounded-md aspect-video object-cover"
+        width={300}
+        height={200}
+      />
 
       <div className="flex flex-col gap-4">
         <h6 className="font-semibold text-xl">{title}</h6>
         <p>{description}</p>
-        <a href={url}>Visit</a>
+        <a href={url} className="flex items-center gap-2">
+          Visit <IconExternalLink />
+        </a>
       </div>
     </article>
   )
