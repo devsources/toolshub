08abae0ee77892ff4398b2162519fb6001c44c18
genaroibc <bonavitaismael@gmail.com>
2023-06-29 14:01:47 -0300

    initial commit

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+export default function Home() {
+  return (
+    <main className="max-w-7xl mx-auto">
+      <div className="flex flex-col gap-4 py-24">
+        <h1 className="font-bold text-center text-6xl">Devsources</h1>
+        <h2 className="font-bold text-center text-3xl">
+          Resources for developers
+        </h2>
+      </div>
+    </main>
+  )
+}
