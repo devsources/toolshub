3aeeaad5165f00ab5768510830383e8afd9e86c0
genaroibc <bonavitaismael@gmail.com>
2023-06-30 16:35:05 -0300

    feat: style category cards

--- app/components/CategoriesSection.tsx
+++ app/components/CategoriesSection.tsx
@@ -1,14 +1,27 @@
 import { Category } from '@/app/components/shared/Category'
 import categories from '@/app/db/categories.json'
 
+const CATEGORY_CARD_GRADIENTS = [
+	'bg-gradient-to-bl from-pink-500 via-red-500 to-yellow-500',
+	'bg-gradient-to-tr from-white via-sky-500 to-sky-500',
+	'bg-gradient-to-bl from-emerald-500 to-lime-600',
+	'bg-gradient-to-bl from-slate-900 via-purple-900 to-slate-900'
+]
+
 export function CategoriesSection() {
 	return (
 		<section>
 			<h3 className='font-semibold text-center text-4xl'>Categories</h3>
 
-			<section className='grid grid-cols-[repeat(auto-fill,minmax(600px,1fr))] gap-4 my-20'>
-				{categories.map(({ description, name, slug }) => (
-					<Category key={name} description={description} name={name} slug={slug} />
+			<section className='grid grid-cols-[repeat(auto-fill,minmax(450px,1fr))] gap-4 my-20'>
+				{categories.map(({ description, name, slug }, index) => (
+					<Category
+						key={name}
+						description={description}
+						name={name}
+						slug={slug}
+						className={CATEGORY_CARD_GRADIENTS[index]}
+					/>
 				))}
 			</section>
 		</section>
--- app/components/shared/Category.tsx
+++ app/components/shared/Category.tsx
@@ -1,26 +1,21 @@
 type Props = {
-  name: string
-  description: string
-  slug: string
+	name: string
+	description: string
+	slug: string
+	className?: string
 }
 
-export function Category({ description, name, slug }: Props) {
-  return (
-    <a href={`/${slug}`}>
-      <section className="flex gap-2 bg-slate-800 rounded-md overflow-hidden">
-        <img
-          src="https://picsum.photos/300"
-          width={200}
-          height={200}
-          alt={name}
-          className=""
-        />
+export function Category({ description, name, slug, className }: Props) {
+	return (
+		<a href={`/${slug}`}>
+			<section className={`flex gap-2 bg-gradient-to-r rounded-md overflow-hidden ${className}`}>
+				<img src='https://picsum.photos/300' width={200} height={200} alt={name} className='' />
 
-        <div className="flex flex-col gap-4 p-6">
-          <h5 className="font-semibold text-2xl">{name}</h5>
-          <p className="text-base">{description}</p>
-        </div>
-      </section>
-    </a>
-  )
+				<div className='flex flex-col gap-4 p-4'>
+					<h5 className='font-semibold text-2xl'>{name}</h5>
+					<p className='text-base'>{description}</p>
+				</div>
+			</section>
+		</a>
+	)
 }
