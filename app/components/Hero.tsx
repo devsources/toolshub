c36cd142a315fcc38bda276a6b877d5ed37f70e6
genaroibc <bonavitaismael@gmail.com>
2023-06-30 16:14:06 -0300

    feat: style Hero

---
+++ app/components/Hero.tsx
@@ -0,0 +1,10 @@
+export function Hero() {
+	return (
+		<section className='grid place-content-center py-64 gap-14'>
+			<h1 className='font-extrabold text-center text-7xl'>Everything you need here</h1>
+			<h2 className='font-semibold text-center text-2xl max-w-prose mx-auto'>
+				The best resources for developers, designers and creatives.
+			</h2>
+		</section>
+	)
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,11 @@
+import { Hero } from "./components/Hero"
 import { Category } from "./components/shared/Category"
 import categories from "@/app/db/categories.json"
 
 export default function Home() {
   return (
     <main className="max-w-7xl mx-auto">
-      <section className="grid place-content-center gap-4 py-24 h-[calc(100vh_-_64px)]">
-        <h1 className="font-extrabold text-center text-7xl">Devsources</h1>
-        <h2 className="font-semibold text-center text-2xl max-w-2xl">
-          Welcome to the platform where you will find the best resources for
-          programmers, designers and more.
-        </h2>
-      </section>
+      <Hero />
 
       <h3 className="font-semibold text-center text-4xl">Categories</h3>
 
