341fb004d6e43ebb22e718de05fd2920260530de
genaroibc <bonavitaismael@gmail.com>
2023-07-02 23:18:43 -0300

    feat: add search input in category page

--- app/[category]/page.tsx
+++ app/[category]/page.tsx
@@ -1,14 +1,7 @@
 import { sluglify, unsluglify } from '@/app/utils/sluglify'
-import { ResourceCard } from '../components/shared/ResourceCard'
+import { Search } from '@/app/[category]/components/Search'
 import categories from '@/app/db/categories.json'
-
-type Resource = {
-	description: string
-	name: string
-	image: string
-	title: string
-	url: string
-}
+import { Resource } from '@/app/types'
 
 type Props = {
 	params: { category: string }
@@ -21,17 +14,7 @@ export default async function Page({ params: { category } }: Props) {
 		<main className='max-w-7xl my-20 mx-auto'>
 			<h1 className='font-bold text-4xl text-center'>{unsluglify(category)}</h1>
 
-			<section className='grid grid-cols-[repeat(auto-fill,minmax(300px,1fr))] gap-x-6 gap-y-8 my-20'>
-				{resources.map(({ description, name, image, title, url }) => (
-					<ResourceCard
-						key={name}
-						description={description}
-						imageURL={image}
-						title={title}
-						url={url}
-					/>
-				))}
-			</section>
+			<Search resources={resources} />
 		</main>
 	)
 }
