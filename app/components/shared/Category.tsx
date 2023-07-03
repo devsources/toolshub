e290a92771f20977ef1eba02b15cc3626ac53ecb
genaroibc <bonavitaismael@gmail.com>
2023-07-02 22:08:30 -0300

    feat: improve category cards design and responsive

--- app/components/shared/Category.tsx
+++ app/components/shared/Category.tsx
@@ -7,14 +7,18 @@ type Props = {
 
 export function Category({ description, name, slug, className }: Props) {
 	return (
-		<a href={`/${slug}`}>
-			<section className={`flex gap-2 bg-gradient-to-r rounded-md overflow-hidden ${className}`}>
-				<img src='https://picsum.photos/300' width={200} height={200} alt={name} className='' />
-
-				<div className='flex flex-col gap-4 p-4'>
+		<a href={`/${slug}`} className='hover:no-underline'>
+			<section
+				className={`flex flex-col justify-between gap-4 bg-gradient-to-r rounded-md overflow-hidden h-full p-4 ${className}`}
+			>
+				<div className='flex flex-col gap-4'>
 					<h5 className='font-semibold text-2xl'>{name}</h5>
-					<p className='text-base'>{description}</p>
+					<p className='text-base text-ellipsis'>{description}</p>
 				</div>
+
+				<button className='bg-white text-gray-900 py-2 px-4 text-sm rounded-full w-fit'>
+					Visit section
+				</button>
 			</section>
 		</a>
 	)
